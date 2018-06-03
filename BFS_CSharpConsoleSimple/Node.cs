950987e872ecb213aa6080e6d99ff62730bd9e55
ltvch <HrenHrenov@myrambler.ru>
2018-06-03 15:19:05 +0300

    Make BFS and DFS realisation on Node class

---
+++ BFS_CSharpConsoleSimple/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFS_CSharpConsoleSimple
+{
+    public class Node
+    {
+        public Node left;
+        public Node right;
+        public String data;
+
+        public Node( String data, Node left, Node right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Node(String data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
--- BFS_CSharpConsoleSimple/Program.cs
+++ BFS_CSharpConsoleSimple/Program.cs
@@ -3,13 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace BFS_CSharpConsoleSimple
 {
     class Program
     {
+        static Node InitialTree()
+        {
+            Node root = new Node("A",
+                new Node("B",
+                    new Node("C"), new Node("D")),
+                new Node("E",
+                    new Node("F"), new Node("G", 
+                                    new Node("H"), null)));
+
+            return root;
+        }
+
         static void Main(string[] args)
         {
+            Node tree = InitialTree();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("BFS is --> ");
+            TraversialBFS(tree);
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("DFS is --> ");
+            TraversialDFS(tree);
+
+            Console.ReadLine();
+        }
+
+        private static void TraversialDFS(Node node)
+        {
+            if (node == null)
+                return;
+
+            Console.Write(node.data + " ");
+            TraversialDFS(node.left);
+            TraversialDFS(node.right);
+
+            //Thread.Sleep(2000);
+        }
+
+        private static void TraversialBFS(Node node)
+        {
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(node);
+            while(queue.Count > 0)
+            {
+                node = queue.Dequeue();
+                Console.Write(node.data + " ");
+
+                if (node.left != null)
+                    queue.Enqueue(node.left);
+
+                if (node.right != null)
+                    queue.Enqueue(node.right);
+
+                //Thread.Sleep(2000);
+            }
         }
     }
 }
